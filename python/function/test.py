cdd2f66f1e42125c333dabd2b3af7e7200b2f482
change jihunpak-private <jihun.pak@cognex.com>
2022-05-17 15:46:20 +0900

    update pytest testfolder

--- python/function/test.py
+++ python/function/test.py
@@ -3,13 +3,17 @@ import numpy as np
 
 def test(a, b):
     result = a + b
-    print("Hello, jihun")
-    print(result)
-    print(np.random.rand(5))
+    return result
 
+
+def test2(a):
+    result = "hello " + a
     return result
 
 
 if __name__ == "__main__":
-    res = test(2, 3)
-    print(res)
+    result = test(2, 3)
+    result2 = test2("jihun")
+    print(result)
+    print(result2)
+    print(np.random.rand(5))
---
+++ python/test/test_str.py
@@ -0,0 +1,5 @@
+from function import test
+
+
+def test_answer():
+    assert test.test2("alex") == "hello alex"
