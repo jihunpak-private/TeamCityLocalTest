cc1b2a04e5123039f7d631535268e3718b97fa94
Jihun Pak <pj522h@gmail.com>
2022-05-19 15:36:59 +0900

    1.3.0

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="jihun-test",
-    version="1.2.9",
+    version="1.3.0",
     author="jihun",
     author_email="pj522h@gmail.com",
     description="Simple Packaging by Python, jihun",
